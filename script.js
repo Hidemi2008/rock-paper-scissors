71e5cdf1645c09473215a30d3c9c109c6174937a
Hidemi2008 <Gabrielhidemi2@gmail.com>
2024-10-03 20:02:27 -0400

    Update script

--- script.js
+++ script.js
@@ -1,55 +1,118 @@
-function getCommputerChoice (min, max){
-    min = Math.ceil(min);
-    max = Math.floor(max);
-   let num = Math.floor(Math.random() * (max - min) + min);
+function playGame (){
 
-   if(num === 0){
-    return "Tesoura"
-   }else{
-    if(num === 1){
-        return "Pedra"
-    }else{
-        return "Papel"
-    }
-   }
-}
-let escolhaComputer = getCommputerChoice(0,3)
-console.log(getCommputerChoice(0,3))
-
-function getHumanChoice (escolha){
-    if(escolha === "PEDRA"){
-        return "Pedra"
-    }else{
-        if(escolha === "TESOURA"){
-            return "Tesoura"
+for (let n = 0; n <5; n++){
+    function getCommputerChoice (min, max){
+        min = Math.ceil(min);
+        max = Math.floor(max);
+       let num = Math.floor(Math.random() * (max - min) + min);
+    
+       if(num === 0){
+        return "Tesoura"
+       }else{
+        if(num === 1){
+            return "Pedra"
         }else{
             return "Papel"
         }
+       }
     }
-}
+    
+    function getHumanChoice (escolha){
+        if(escolha === "PEDRA"){
+            return "Pedra"
+        }else{
+            if(escolha === "TESOURA"){
+                return "Tesoura"
+            }else{
+                return "Papel"
+            }
+        }
+    }
+    
+    
+    
+    function playRound (humanChoise, computerChoise) {
+    
+    humanChoise = humanChoise.toUpperCase()
+    computerChoise = computerChoise.toUpperCase()
 
-let escolha = prompt("Qual a sua escolha ? ")
-    escolha = escolha.toUpperCase()
-
-console.log(getHumanChoice(escolha))
-
-let humanScore = 0
-let computerScore = 0
-
-function playRound (humanChoise, computerChoise) {
-    if(humanChoise === "Tesoura" && computerChoise === "Papel"){
+    
+    if(humanChoise === "TESOURA" && computerChoise === "PAPEL"){
         return humanScore++
     }else{
-        if(humanChoise === "Pedra" && computerChoise === "Tesoura"){
+        if(humanChoise === "PEDRA" && computerChoise === "TESOURA"){
             return humanScore++
         }else{
-            if(humanChoise === "Papel" && computerChoise === "Pedra"){
+            if(humanChoise === "PAPEL" && computerChoise === "PEDRA"){
                 return humanScore++
             }else{
-                return computerScore++
+                if(humanChoise === computerChoise){
+                    return empate++
+                }else{
+                    return computerScore++
+                }
             }
         }
     }
 
+
+    }
+
+    let escolhaComputer = getCommputerChoice(0,3)
+    console.log(escolhaComputer)
+    
+    
+    let escolha = prompt("Qual a sua escolha ? ")
+        escolha = escolha.toUpperCase()
+    let escolhaHumana = getHumanChoice(escolha)
+    console.log(escolhaHumana)
+    
+    let humanScore = 0
+    let computerScore = 0
+    let empate = 0
+    
+    let rodada = playRound(escolhaHumana,escolhaComputer)
+    
+    console.log(humanScore)
+    console.log(computerScore)
+    console.log(empate)
+
+    if (humanScore > 2){
+        console.log(`Você empatou, tente novamente!!!`)
+        return `Parabéns você venceu!!!`
+        
+    }else{
+        if(computerScore > 2){
+            console.log(`Você empatou, tente novamente!!!`)
+            return `Você perdeu !!!`
+        }else{
+            if(humanScore === 2 && computerScore === 2 && empate === 1){
+                console.log(`Você empatou, tente novamente!!!`)
+                return `Você empatou, tente novamente!!!`
+            }else{
+                if(empate > 2){
+                        console.log(`Você empatou, tente novamente!!!`)
+                        return `Você empatou, tente novamente!!!`
+                }
+            }
+        }
+    }
+
+}}
+
+console.log(playGame())
+/*
+if (humanScore > 2){
+    console.log(`Parabéns você venceu!!!`)
+}else{
+    if(computerScore > 2){
+        console.log(`Você perdeu !!!`)
+    }else{
+        if(humanScore === computerScore){
+            console.log(`Você empatou, tente novamente!!!`)
+        }else{
+            console.log(`Você empatou, tente novamente!!!`)
+        }
+    }
 }
-console.log(playRound(escolha,escolhaComputer))
+    */
